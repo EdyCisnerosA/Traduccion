71abbbf971b8215ea9426a66327aff0111e2fca5
Edilzar Cisneros Ancheyta <edy.cisneros1893@gmail.com>
2017-08-01 14:53:39 -0500

    Recuperand codigo

--- cadena.c
+++ cadena.c
@@ -1,6 +1,18 @@
- 	
+  	
 #include <libintl.h>
 #include <locale.h>
 
-#define _(cadena) gettext(aaaaaaaaaaaaaa no se que verga hice
+#define _(cadena) gettext(cadena)
+
+int main(int argc, const char *argv[]){
+	bind_textdomain_codeset ("codigo", "UTF-8");
+	setlocale(LC_ALL,"");
+	bindtextdomain("codigo", "idioma");
+	textdomain("codigo");
+	char cadena[] = "Hola mundo piñata!";
+
+	printf(_("Hola alumnos Intel-Buap\n"));
+
+	return 0;
+}
 
